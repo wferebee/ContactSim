897d262340cdcd99ade4ba501eb342eebd0132c6
William Ferebee <williampferebee@gmail.com>
2022-08-02 03:38:03 -0400

    missed a TODO comment

--- LiteDbSetup/LiteDbOptions.cs
+++ LiteDbSetup/LiteDbOptions.cs
@@ -2,7 +2,6 @@
 {
     public class LiteDbOptions
     {
-        //Delete this?
         public const string DatabaseOptions = "LiteDbOptions";
         public string DatabaseLocation { get; set; } = String.Empty;
     }
